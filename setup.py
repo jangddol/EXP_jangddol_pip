c88ab06becea999ee4f2bdb4dc69ce7b9ab57559
jangddol <j991005@korea.ac.kr>
2021-12-05 15:26:44 +0900

    p 1.0.0
    first push

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+      name='exp_jangddol',
+      version='p 1.0.0',
+      url='https://github.com/jangddol/EXP_jangddol_pip.git',
+      author='jangddol',
+      author_email='j991005@korea.ac.kr',
+      install_requires=[
+          'uncertainties==3.1.6',
+          'matplotlib==3.5.0',
+          'numpy==1.21.0'
+      ]
+)
